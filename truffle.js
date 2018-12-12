d1515a12c944688ee74294325feac09cddeb1bf1
smallThinking <wnsdnek778@gmail.com>
2018-12-12 21:05:42 +0900

    truffle config setting

---
+++ truffle.js
@@ -0,0 +1,56 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+// Store environment-specific variable from '.env' to process.env
+require('dotenv').config();
+
+module.exports = {
+    networks: {
+        // testnets
+        // properties
+        // network_id: identifier for network based on ethereum blockchain. Find out more at https://github.com/ethereumbook/ethereumbook/issues/110
+        // gas: gas limit
+        // gasPrice: gas price in gwei
+
+        development: {
+            host: 'localhost',
+            port: 8545,
+            network_id: '*' // Match any network id
+        },
+        ganache_gui: {
+            host: 'localhost',
+            port: '7545',
+            network_id: '5777'
+        },
+        // ganache: {
+        //     provider: () => new HDWalletProvider(process.env.MNEMONIC, "http://127.0.0.1:7545"),
+        //     network_id: 5777,
+        //     gas: 6700000,
+        //     //gasPrice: 21
+        // },
+        ropsten: {
+            provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY),
+            network_id: 3,
+            gas: 6700000,
+            //gasPrice: 21
+        },
+        kovan: {
+            provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://kovan.infura.io/v3/" + process.env.INFURA_API_KEY),
+            network_id: 42,
+            gas: 6700000,
+            //gasPrice: 21
+        },
+        rinkeby: {
+            provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY),
+            network_id: 4,
+            gas: 6700000,
+            //gasPrice: 21
+        },
+        // main ethereum network(mainnet)
+        main: {
+            provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY),
+            network_id: 1,
+            gas: 6700000,
+            //gasPrice: 21
+        }
+    }
+}
